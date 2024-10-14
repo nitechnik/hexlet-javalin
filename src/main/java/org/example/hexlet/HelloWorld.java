8d53469eb0cf1df9f8106bfc0f49b6eaae0e2151
Nikolai Tabakov <itechnik.swd@gmail.com>
2024-10-14 22:06:26 +0300

    Automatic conversion

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,10 +4,13 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаём приложение
-        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+        app.start(7070);
     }
 }
